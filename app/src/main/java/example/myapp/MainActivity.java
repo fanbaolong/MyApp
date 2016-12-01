a23dd08362d70999cafc09e1b20cc8df7312615e
787163262@qq.com <fbl19911219>
2016-12-01 11:41:00 +0800

    first commit

--- app/src/main/java/example/myapp/MainActivity.java
+++ app/src/main/java/example/myapp/MainActivity.java
@@ -12,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+
     }
 }
