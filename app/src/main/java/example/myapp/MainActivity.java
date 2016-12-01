b2da1ca503f374f17ffdb8c1596330c0fc3cd58f
787163262@qq.com <fbl19911219>
2016-12-01 11:38:13 +0800

    first commit

--- app/src/main/java/example/myapp/MainActivity.java
+++ app/src/main/java/example/myapp/MainActivity.java
@@ -3,6 +3,9 @@ package example.myapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
